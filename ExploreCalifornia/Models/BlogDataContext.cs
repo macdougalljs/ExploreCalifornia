9b57c7cb29b5177c4d83985eef6cb755d5f2f6a0
Jeremy MacDougall <macdojer@outlook.com>
2022-04-26 19:07:26 -0400

    Enhancing a view component with logic and dynamic markup

--- ExploreCalifornia/Models/BlogDataContext.cs
+++ ExploreCalifornia/Models/BlogDataContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace ExploreCalifornia.Models
 {
@@ -8,6 +9,39 @@ namespace ExploreCalifornia.Models
         {
             get; set;
         }   // naming is super important as DBSet<T> allows entity framework to directly access the table "Posts" in the database
+        public IQueryable<MonthlySpecial> MonthlySpecials
+        {
+            get
+            {
+                return new[]
+                {
+                    new MonthlySpecial {
+                        Key = "calm",
+                        Name = "California Calm Package",
+                        Type = "Day Spa Package",
+                        Price = 250,
+                    },
+                    new MonthlySpecial {
+                        Key = "desert",
+                        Name = "From Desert to Sea",
+                        Type = "2 Day Salton Sea",
+                        Price = 350,
+                    },
+                    new MonthlySpecial {
+                        Key = "backpack",
+                        Name = "Backpack Cali",
+                        Type = "Big Sur Retreat",
+                        Price = 620,
+                    },
+                    new MonthlySpecial {
+                        Key = "taste",
+                        Name = "Taste of California",
+                        Type = "Tapas & Groves",
+                        Price = 150,
+                    },
+                }.AsQueryable();
+            }
+        }
 
         public BlogDataContext(DbContextOptions<BlogDataContext> options) : base(options)
         {
--- ExploreCalifornia/ViewComponents/MonthlySpecialsViewComponent.cs
+++ ExploreCalifornia/ViewComponents/MonthlySpecialsViewComponent.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExploreCalifornia.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace ExploreCalifornia.ViewComponents
 {
     [ViewComponent] // or
     public class MonthlySpecialsViewComponent : ViewComponent // or this
     {
-        public string Invoke()
+        private readonly BlogDataContext db;
+
+        public MonthlySpecialsViewComponent(BlogDataContext db)
         {
-            return "TODO: Show Monthly Specials";
+            this.db = db;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var specials = db.MonthlySpecials.ToArray();
+            return View(specials);
         }
     }
 }
