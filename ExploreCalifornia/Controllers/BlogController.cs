8159910a3cdc8dc6405bf5671415e93006be74ba
Jeremy MacDougall <macdojer@outlook.com>
2022-04-26 14:07:34 -0400

    Read from database using Entity Framework

--- ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -1,35 +1,31 @@
 ﻿using ExploreCalifornia.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Linq;
 
 namespace ExploreCalifornia.Controllers
 {
     [Route("blog")]  // be careful, this removes it as being a candidate for more generic routes defined in the startup class though usually desireable
     public class BlogController : Controller
     {
+        private readonly BlogDataContext _db;
+        public BlogController(BlogDataContext db)
+        {
+            _db = db;
+        }
         //GET: /<controller>/
         [Route("")]
         public IActionResult Index()
         {
-            return View();
+            var posts = _db.Posts.OrderByDescending(x => x.Posted).Take(5).ToArray();
+            return View(posts);
         }
 
         [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
         public IActionResult Post(int year, int month, string key)
         {
-            var post = new Post
-            {
-                Title = "My blog post",
-                Posted = DateTime.Now,
-                Author = "Jeremy MacDougall",
-                Body = "This is a great blog post, don't you think?",
-            };
-            /*
-            ViewBag.Title = "My blog post";
-            ViewBag.Posted = DateTime.Now;
-            ViewBag.Author = "Jeremy MacDougall";
-            ViewBag.Body = "This is a great blog post, don't you think?";
-            */
+            var post = _db.Posts.FirstOrDefault(x => x.Key == key);
+          
             return View(post);
         }
 
@@ -42,15 +38,20 @@ namespace ExploreCalifornia.Controllers
         [HttpPost, Route("create")]
         public IActionResult Create(Post post)
         {
-          if (ModelState.IsValid)
-                return View(); // keep processing the request
+            if (!ModelState.IsValid)
+                return View();
 
             post.Author = User.Identity.Name;
             post.Posted = DateTime.Now;
-            return View();
 
+            _db.Posts.Add(post);
+            _db.SaveChanges();
+            return RedirectToAction("Post", "Blog", new
+            {
+                year = post.Posted.Year,
+                month = post.Posted.Month,
+                key = post.Key
+            });
         }
-
-       
     }
 }
